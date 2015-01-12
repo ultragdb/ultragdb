cf012f9bdfac47bed57982bef3dc700b371ded58
Xu,Chiheng(徐持恒) <chiheng.xu@gmail.com>
2015-01-12 21:43:11 +0800

    org.eclipse.cdt.common.Encoding : add some test code, commented out

--- common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/Encoding.java
+++ common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/Encoding.java
@@ -4,7 +4,23 @@ import java.nio.charset.Charset;
 
 public class Encoding {
 	public static Charset UTF_8(){
+//		CharSetTest.main(null);
 		Charset charset = Charset.forName("UTF-8"); //$NON-NLS-1$
 		return charset;
 	}
 }
+
+//class CharSetTest {
+//
+//    public static void main(String[] args) {
+//    	System.out.println("Default Charset=" + Charset.defaultCharset());
+//    	System.out.println("file.encoding=" + System.getProperty("file.encoding"));
+//    	System.out.println("Default Charset in Use=" + getDefaultCharSet());
+//    }
+//
+//    private static String getDefaultCharSet() {
+//    	OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream());
+//    	String enc = writer.getEncoding();
+//    	return enc;
+//    }
+//}
