557911f3f5bffbe55183655cd1e21b3a958604c5
Xu,Chiheng(徐持恒) <chiheng.xu@gmail.com>
2015-01-18 23:59:12 +0800

    fix problem that MinGW DLLs can't be found when using Cygwin to MinGW cross compilers

--- common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/ProcessFactory.java
+++ common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/ProcessFactory.java
@@ -3,8 +3,10 @@ package org.eclipse.cdt.common;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.eclipse.core.runtime.Path;
@@ -12,8 +14,9 @@ import org.eclipse.core.runtime.Platform;
 
 public class ProcessFactory {
 
-	private static Map<String, String> envpToEnvMap(String[] envp) {
-		TreeMap<String, String> environment = new TreeMap<String, String>();
+	private static TreeMap<String, String> envpToEnvMap(String[] envp) {
+		TreeMap<String, String> environment = new TreeMap<String, String>(
+				String.CASE_INSENSITIVE_ORDER);
 		if (envp != null) {
 			for (String envstring : envp) {
 				int eqlsign = envstring.indexOf('=');
@@ -34,11 +37,14 @@ public class ProcessFactory {
 
 		List<String> cmdList = Arrays.asList(cmdarray);
 		ProcessBuilder pb = new ProcessBuilder(cmdList);
+		TreeMap<String, String> environment = new TreeMap<String, String>(
+				String.CASE_INSENSITIVE_ORDER);
 		Map<String, String> env = pb.environment();
+		environment.putAll(env);
+		env.clear();
 		if (envp != null) {
-			Map<String, String> environment = envpToEnvMap(envp);
-			env.clear();
-			env.putAll(environment);
+			TreeMap<String, String> environment1 = envpToEnvMap(envp);
+			environment.putAll(environment1);
 		}
 
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
@@ -50,18 +56,28 @@ public class ProcessFactory {
 			 * @see https://cygwin.com/cygwin-ug-net/using-cygwinenv.html
 			 */
 			String nodosfilewarnings = "nodosfilewarning"; //$NON-NLS-1$
-			String cygwinEnvVarValue = env.get("CYGWIN"); //$NON-NLS-1$
+			String cygwinEnvVarValue = environment.get("CYGWIN"); //$NON-NLS-1$
 			if (cygwinEnvVarValue == null) {
-				env.put("CYGWIN", nodosfilewarnings); //$NON-NLS-1$
+				environment.put("CYGWIN", nodosfilewarnings); //$NON-NLS-1$
 			} else if (cygwinEnvVarValue.indexOf(nodosfilewarnings) == -1) {
 				cygwinEnvVarValue += " " + nodosfilewarnings; //$NON-NLS-1$
-				env.put("CYGWIN", cygwinEnvVarValue); //$NON-NLS-1$
+				environment.put("CYGWIN", cygwinEnvVarValue); //$NON-NLS-1$
+			}
+			String mingwSysroot = null;
+			if (WindowsGCC.isCygwin32()) {
+				mingwSysroot = WindowsGCC.getMingw32CrossGccSysroot();
+			} else if (WindowsGCC.isCygwin64()) {
+				mingwSysroot = WindowsGCC.getMingw64CrossGccSysroot();
+			}
+			if (mingwSysroot != null) {
+				String pathEnvVarValue = environment.get("PATH"); //$NON-NLS-1$
+				pathEnvVarValue += ";" + mingwSysroot + "/mingw/bin";
+				environment.put("PATH", pathEnvVarValue); //$NON-NLS-1$
 			}
 		}
-
 		{
 			boolean setLangEnvVartoDefault = false;
-			String langEnvValue = env.get("LANG"); //$NON-NLS-1$
+			String langEnvValue = environment.get("LANG"); //$NON-NLS-1$
 			if (langEnvValue == null) {
 				setLangEnvVartoDefault = true;
 			} else {
@@ -78,20 +94,58 @@ public class ProcessFactory {
 						charset = "UTF-8"; //$NON-NLS-1$
 					}
 					langEnvValue = country + '.' + charset;
-					env.put("LANG", langEnvValue); //$NON-NLS-1$
-					env.put("LC_ALL", langEnvValue); //$NON-NLS-1$
+					environment.put("LANG", langEnvValue); //$NON-NLS-1$
+					environment.put("LC_ALL", langEnvValue); //$NON-NLS-1$
 				}
 			}
 			if (setLangEnvVartoDefault) {
-				env.put("LANG", "en_US.UTF-8"); //$NON-NLS-1$ //$NON-NLS-2$
-				env.put("LC_ALL", "en_US.UTF-8"); //$NON-NLS-1$ //$NON-NLS-2$
+				environment.put("LANG", "en_US.UTF-8"); //$NON-NLS-1$ //$NON-NLS-2$
+				environment.put("LC_ALL", "en_US.UTF-8"); //$NON-NLS-1$ //$NON-NLS-2$
 			}
 		}
 
+		env.putAll(environment);
 		if (dir != null) {
 			pb.directory(dir);
 		}
+		/*
+		 * for debug purpose
+		 */
+		StringBuilder sb = new StringBuilder();
+		{
+			for (int i = 0; i < cmdarray.length; i++) {
+				if (i != 0) {
+					sb.append(' ');
+				}
+				sb.append('\'');
+				sb.append(cmdarray[i]);
+				sb.append('\'');
+			}
+			sb.append("\n\n");
 
+			Iterator<Entry<String, String>> iterator = environment.entrySet()
+					.iterator();
+			while (iterator.hasNext()) {
+				Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator
+						.next();
+				sb.append(entry.getKey());
+				sb.append('=');
+				sb.append(entry.getValue());
+				sb.append('\n');
+			}
+
+			sb.append("\n");
+			if (dir != null) {
+				sb.append(dir.toString());
+			} else {
+				sb.append("no directory specified");
+			}
+			sb.append("\n");
+		}
+		/*
+		 * set breakpoint on next line to inspect sb when debugging, to see the
+		 * ultimate parameters of ProcessBuilder
+		 */
 		Process p = pb.start();
 		return p;
 	}
--- common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/WindowsGCC.java
+++ common/org.eclipse.cdt.common/src/org/eclipse/cdt/common/WindowsGCC.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 
 public class WindowsGCC {
@@ -15,8 +16,12 @@ public class WindowsGCC {
 	private static boolean isMinGW32;
 	private static boolean isMinGW64;
 
+	private static String mingw32CrossGccSysroot;
+	private static String mingw64CrossGccSysroot;
+
 	static {
 		initialize();
+		initializeMingwCrossGccSysroot();
 	}
 
 	private static String[] execAndGetLines(String[] cmdarray) {
@@ -62,6 +67,32 @@ public class WindowsGCC {
 		return;
 	}
 
+	private static void initializeMingwCrossGccSysroot() {
+		if (isCygwin32) {
+			String[] lines = execAndGetLines(new String[] {
+					"i686-w64-mingw32-gcc", "-print-sysroot" });
+			if (lines == null || lines.length != 1) {
+				return;
+			}
+			String line = lines[0];
+			if (line.length() > 0) {
+				mingw32CrossGccSysroot = Cygwin.cygwinToWindowsPath(Path
+						.fromOSString(line).toPortableString());
+			}
+		} else if (isCygwin64) {
+			String[] lines = execAndGetLines(new String[] {
+					"x86_64-w64-mingw32-gcc", "-print-sysroot" });
+			if (lines == null || lines.length != 1) {
+				return;
+			}
+			String line = lines[0];
+			if (line.length() > 0) {
+				mingw64CrossGccSysroot = Cygwin.cygwinToWindowsPath(Path
+						.fromOSString(line).toPortableString());
+			}
+		}
+	}
+
 	public static boolean isCygwin32() {
 		return isCygwin32;
 	}
@@ -76,5 +107,13 @@ public class WindowsGCC {
 
 	public static boolean isMinGW64() {
 		return isMinGW64;
+	}
+
+	public static String getMingw32CrossGccSysroot() {
+		return mingw32CrossGccSysroot;
+	}
+
+	public static String getMingw64CrossGccSysroot() {
+		return mingw64CrossGccSysroot;
 	}
 }
